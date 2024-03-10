13c7f2ccd09d080f70c79f96c2a6b35ef0dd6f51
João Vítor Valadares <42593399+joaovaladares@users.noreply.github.com>
2024-03-10 00:53:00 -0300

    Update #2 - EvenFibonacciNumbers.hs

--- ProjectEuler/#2 - EvenFibonacciNumbers.hs
+++ ProjectEuler/#2 - EvenFibonacciNumbers.hs
@@ -1,10 +1,10 @@
 module EvenFibonnaciNumbers where
 
 -- Find the sum of all even Fibonacci numbers below 4 million.
--- First idea 
 fibs :: [Integer]
 fibs = 1 : 2 : zipWith (+) fibs (tail fibs)
 
+-- First idea 
 sumEvenFibNumbersBelow4m :: Integer
 sumEvenFibNumbersBelow4m = sum $ filter even $ takeWhile (< 4000000) fibs
 
